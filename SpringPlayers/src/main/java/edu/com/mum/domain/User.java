4a6ccb3786f27417173bd340da8f0505ef4dced0
Ayman Metwali <ayman.metwali@yahoo.com>
2015-08-09 18:08:54 -0500

    user domain and application context

---
+++ SpringPlayers/src/main/java/edu/com/mum/domain/User.java
@@ -0,0 +1,72 @@
+package edu.com.mum.domain;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class User { 
+	
+@Id @GeneratedValue
+	private Long id;
+	
+
+	private String username;
+
+	private String password;
+	
+	
+
+
+
+	public User() {
+
+	}
+
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	
+
+	
+	public String getPassword() {
+		return password;
+	}
+	
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+}
