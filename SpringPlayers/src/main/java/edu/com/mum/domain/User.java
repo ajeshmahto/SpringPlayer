3e13704749f072976ce717fd8079f8854b988c14
Ayman Metwali <ayman.metwali@yahoo.com>
2015-08-10 10:18:25 -0500

    Updating User and UserProfile Enttites

--- SpringPlayers/src/main/java/edu/com/mum/domain/User.java
+++ SpringPlayers/src/main/java/edu/com/mum/domain/User.java
@@ -1,11 +1,15 @@
 package edu.com.mum.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
@@ -27,12 +31,12 @@ public class User {
 	private String password;
 	
 	
-/*	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER, mappedBy="user")
+	@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER, mappedBy="user")
 	private List<Order> orders = new ArrayList<Order>();
 
 
 	@OneToMany(cascade=CascadeType.ALL, mappedBy="user", fetch=FetchType.EAGER)
-	private List<PaymentInfo> paymentInfos = new ArrayList<PaymentInfo>();*/
+	private List<PaymentInfo> paymentInfos = new ArrayList<PaymentInfo>();
 
 
 	@OneToOne(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
@@ -55,17 +59,17 @@ public class User {
 		return id;
 	}
 	
-/*	public List<Order> getOrders() {
+	public List<Order> getOrders() {
 		return orders;
-	}*/
-	
+	}
+
 	public String getPassword() {
 		return password;
 	}
 	
-/*	public List<PaymentInfo> getPaymentInfos() {
+	public List<PaymentInfo> getPaymentInfos() {
 		return paymentInfos;
-	}*/
+	}
 	
 
 
@@ -84,20 +88,20 @@ public class User {
 	}
 
 
-/*	public void setOrders(List<Order> orders) {
+	public void setOrders(List<Order> orders) {
 		this.orders = orders;
 	}
-*/
+
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
 
-/*	public void setPaymentInfos(List<PaymentInfo> paymentInfos) {
+	public void setPaymentInfos(List<PaymentInfo> paymentInfos) {
 		this.paymentInfos = paymentInfos;
 	}
-*/
+
 
 	public void setUsername(String username) {
 		this.username = username;
--- SpringPlayers/src/main/java/edu/com/mum/domain/UserProfile.java
+++ SpringPlayers/src/main/java/edu/com/mum/domain/UserProfile.java
@@ -10,7 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.Size;
+
 
 import org.hibernate.validator.constraints.NotEmpty;
 
